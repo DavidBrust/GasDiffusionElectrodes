4db48dfd20f475ec3fd644ca6e1d290ee10b7c2b
Brust, David <david.brust@dlr.de>
2022-11-11 09:03:18 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/GasDiffusionElectrodes.jl
@@ -0,0 +1,5 @@
+module GasDiffusionElectrodes
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using GasDiffusionElectrodes
+using Test
+
+@testset "GasDiffusionElectrodes.jl" begin
+    # Write your tests here.
+end
